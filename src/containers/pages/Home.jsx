025f166cb46ae992628cff359b4afaa704d93005
ortizfram <ortizfranco48@gmail.com>
2023-08-22 10:32:23 -0300

    update past commit

--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -2,14 +2,14 @@ import FullWithLayout from "hocs/layouts/FullWithLayout";
 import { connect } from "react-redux";
 import React from "react";
 import { Link } from "react-router-dom";
-import WantDemo from 'components/home/WantDemo'
+import WantDemoForm from 'components/home/WantDemoForm'
 import Header from "components/home/Header";
 
 function Home({}) {
   return (
     <FullWithLayout>
       <Header />
-      <WantDemo />
+      <WantDemoForm />
     </FullWithLayout>
   );
 }
